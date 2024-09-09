bfd45033673775fa42e91ffc2113847845c717a5
AtlasVIM <caldasmartim@gmail.com>
2024-09-09 12:59:22 +0100

    changed page headers to fit length

---
+++ client/public/markdown/content/npcs/elaine-izinski.md
@@ -0,0 +1,59 @@
+# Personal Aspects
+
+
+## Personality
+
+
+She is a gentle, kind and patient soul. Always willing to listen to another person’s strife.
+
+## Likes
+
+Flowers. Nature. Ashtorethia.
+
+## Dislikes
+
+Rude People, egotistical people
+
+## Motivations
+
+Learn more about the world. Live in comunion with nature
+
+## Fears
+
+Losing Ash.
+
+# Biography
+
+## Birth
+
+Elaine was born in 4th of Isoles of the year 80 R.E. in Chesnoya, Kaczyslaw, a village surrounded by woods and sunflower fields.
+
+## Infancy
+
+During infancy, Elaine spent a lot of her time in the woods and fields of Chesnoya, reading books or playing with the children of her village. This gave her a profound appreciation for nature.
+
+## Adult Life
+
+### 99 E.R. -
+
+Elaine started her attendance in [Leviathan University](../../world/leviathan_university)’s biology course where a short time after, in the 7th of Irides of 99 E.R, she and Ashtorethia, a girl she met during that year, started dating.
+
+# Philosophy
+
+## Personal
+
+Be good to the world around you and it will be good to you in return.
+
+## Political
+
+Elaine is not a very political person except when it comes with Environmentalism, she believes that the protection of nature is supreme and that civilization and technology must be used in communion with it.
+
+# Relations
+
+## Friends
+
+Ashtorethia and her friends - **Sometimes she shares magical (narcotic) herbs she raises with her friends.**
+
+## Rivals
+
+[Blair Alexander](./blair_alexander) - ?
--- client/src/db/page-items/npcsdb.js
+++ client/src/db/page-items/npcsdb.js
@@ -1,4 +1,8 @@
 import damienShamrockCover from '../../assets/covers/damien_shamrock.png'
+import vascoRepresasCover from '../../assets/covers/vasco_represas.png'
+import idrisYashantiCover from '../../assets/covers/idris_yashanti.png'
+import wujinLeeCover from '../../assets/covers/wujin_lee.png'
+import elaineIzinskiCover from '../../assets/covers/elaine_izinski.png'
 
 import { ga, gc, npc} from '../misc/vardb'
 
@@ -13,5 +17,63 @@ export const npcDB = [
             type: [npc],
         },
         quote: 'For a brighter tomorrow'
-    }
+    },
+    {
+        name: 'Vasco Represas',
+        to:'vasco_represas',
+        cover: vascoRepresasCover,
+        tables :{
+            celestialRegion: [gc],
+            type: [npc],
+            group: 'Skaterboyz'
+        },
+        quote: '"Woah, radical, dude."'
+    },
+    {
+        name: 'Idris Yashanti',
+        to:'idris_yashanti',
+        cover: idrisYashantiCover,
+        tables :{
+            celestialRegion: [gc],
+            type: [npc],
+            group: ['Leviathan University Faculty'],
+            sexuality: ['Homossexual']
+        },
+        quote: 'Mystery brings curiosity. Curiosity brings knowledge. Knowledge brings power.'
+    },
+    {
+        name: 'Wu-Jin Lee',
+        to:'wujin_lee',
+        cover: wujinLeeCover,
+        tables :{
+            celestialRegion: [gc],
+            type: [npc],
+            group: ['Skaterboyz']
+        },
+        quote: '"Bad ideas don\'t exist, only unique experiences."'
+    },
+    {
+        name: 'Elaine Izinski',
+        to:'elaine_izinski',
+        md: 'npcs/elaine-izinski',
+        cover: elaineIzinskiCover,
+        tables :{
+            celestialRegion: [gc],
+            type: [npc],
+            birth: ['4th of Isoles, 80 R.E.'],
+            gender: ['Female'],
+            Sexuality: ['Homossexual'],
+            Height:['1,59m'],
+            Eyes:['Black'],
+            Hair: ['Pink'],
+            Occupations: ['Student'],
+            Reasoning: ['Ethos'],
+            Reaction: ['Fight']
+
+            
+        },
+        quote: '"Flowers bloom for you."'
+    },
+
+
 ]
